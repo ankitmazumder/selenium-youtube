7e5c67e9cedeb07b1c7de8739ee46bb6b2d1989f
Ankit Mazumder <38405877+ankitmazumder@users.noreply.github.com>
2022-01-17 05:33:44 +0000

    writing the parsed data into csv

--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -15,6 +16,24 @@ def get_videos(driver):
     driver.get(YOUTUBE_TRENDING_URL)
     videos = driver.find_elements(By.TAG_NAME, VIDEO_DIV_TAG)
     return videos
+def parse_video(video):
+  title_tag = video.find_element(By.ID,'video-title')
+  title = title_tag.text
+  url = title_tag.get_attribute('href')
+  thumbnail_tag = video.find_element(By.TAG_NAME,'img')
+  thumbnail_url = thumbnail_tag.get_attribute('src')
+
+  channel_div = video.find_element(By.CLASS_NAME,'ytd-channel-name')
+
+  channel_name = channel_div.text
+  description = video.find_element(By.ID,'description-text').text
+  return{
+    'title': title,
+    'url': url,
+    'thumbnail_url': thumbnail_url,
+    'channel': channel_name,
+    'description': description
+  }
 if __name__ == "__main__":
   print('Creating Driver...')
   driver=getDriver()
@@ -26,9 +45,9 @@ videos = get_videos(driver)
 
 print(f'Found {len(videos)}videos')
 
-print('Parsing the First Video')
-
-video = videos[0]
-title_tag = video.find_elements(By.ID,'video-title')
-title = title_tag.text
-print(title)
+print('Parsing top 10 Video')
+videos_data = [parse_video(video) for video in videos[:10]]
+print("Saving the data into CSV")
+videos_df=pd.DataFrame(videos_data)
+print(videos_df)
+videos_df.to_csv("Trending.csv",index=None)
